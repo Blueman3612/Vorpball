a08d9661e1b1bfd85c29d52cd4d90d1c7ee47ac5
Blueman3612 <nathan.hall87@gmail.com>
2025-01-30 01:22:29 -0500

    Fuck Vercel 4

--- supabase/functions/get_sorted_players/index.ts
+++ supabase/functions/get_sorted_players/index.ts
@@ -2,21 +2,45 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-// Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { createClient } from '@supabase/supabase-js'
 
 console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+// @deno-types="https://deno.land/x/types/globals.d.ts"
+declare const Deno: any;
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+Deno.serve(async (req: Request) => {
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser()
+
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await supabaseClient.from('players').select('*')
+    if (error) throw error
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (err: any) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
 })
 
 /* To invoke locally:
@@ -25,8 +49,7 @@ Deno.serve(async (req) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/get_sorted_players' \
-    --header 'Authorization: Bearer ' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --header 'Authorization: Bearer YOUR_AUTH_TOKEN' \
+    --header 'Content-Type: application/json'
 
 */
